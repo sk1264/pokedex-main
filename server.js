8b6f0d3298e3cf48fcfc4bef24e5543c434883dc
sk1264 <samanthakaras1@gmail.com>
2023-04-05 19:55:42 -0400

    server updated

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const express = require('express');
 const app = express();
 
 const pokemon = require("./models/pokemon");
+const methodOverride = require('method-override');
 
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(methodOverride('_method'));
+
+// index
 app.get('/pokemon/', (req, res) => {
     res.render('index.ejs', {pokemon, pokemon})
 })
 
+// new
+app.get('/pokemon/new/', (req, res) => {
+    res.render("new.ejs", {title: "Add New Pokemon"});
+})
 
-
+// listening
 app.listen(3000, () => {
     console.log('listening on port 3000');
 })
