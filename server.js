fd5972d8ff919d33887a9158d960943a46e3a84f
sk1264 <samanthakaras1@gmail.com>
2023-04-06 16:46:22 -0400

    changes

--- server.js
+++ server.js
@@ -48,9 +48,6 @@ app.post('/pokemon', (req, res) => {
         hp: req.body.hp,
         attack: req.body.attack,
         defense: req.body.defense,
-        spattack: req.body.spattack,
-        spdefense: req.body.spdefense,
-        speed: req.body.speed,
       }}
       pokemon.push(newPokemon);
       res.redirect("/pokemon");
