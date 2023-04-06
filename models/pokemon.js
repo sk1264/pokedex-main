920268653b61ac2c908cae3e92b3551c68cdc21e
sk1264 <samanthakaras1@gmail.com>
2023-04-06 12:29:15 -0400

    update

--- models/pokemon.js
+++ models/pokemon.js
@@ -50958,3 +50958,4 @@ module.exports = [
     }
   }
 ];
+
--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(methodOverride('_method'));
 
+// setup
+app.get('/', (req, res) => {
+    res.send("App is working")
+})
+
 // index
 app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {pokemon, pokemon})
@@ -17,21 +22,27 @@ app.get('/pokemon', (req, res) => {
 // show
 
 
+
 // new
 app.get('/pokemon/new', (req, res) => {
     res.render("new.ejs");
 })
 
 // edit
-app.get('/:id/edit', (req,res) => {
-    res.render("edit.ejs")
-})
+// app.get('/pokemon/:id', (req,res) => {
+//     res.render('edit.ejs',
+//     pokemon: pokemon)
+// })
 
 // create
 
 // update
 
-// destroy
+// delete
+app.delete('/pokemon/:id', (req, res) => {
+    pokemon.splice(req.params.id, 1)
+    res.redirect('/pokemon');
+})
 
 // listening
 app.listen(3000, () => {
